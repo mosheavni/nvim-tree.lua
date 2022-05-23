0a7d0ca1b7070c9e8961e32c7b6e97c1f0505471
Moshe Avni <mavni@netapp.com>
2022-05-23 14:25:03 +0300

    fix

--- lua/nvim-tree/actions/init.lua
+++ lua/nvim-tree/actions/init.lua
@@ -8,7 +8,7 @@ local nvim_tree_callback = require("nvim-tree.config").nvim_tree_callback
 
 local M = {
   mappings = {
-    { key = "M", action = "open_popup_menu"},
+    { key = "M", action = "open_popup_menu" },
     { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
     { key = "<C-e>", action = "edit_in_place" },
     { key = "O", action = "edit_no_picker" },
@@ -195,14 +195,16 @@ local function merge_mappings(user_mappings)
     if type(map.key) == "table" then
       local filtered_keys = {}
       for _, key in pairs(map.key) do
-        if not vim.tbl_contains(user_keys, key:lower()) and not vim.tbl_contains(removed_keys, key:lower()) then
+        local lhs = string.find(key:lower(), "^<.*>$") and key:lower() or key
+        if not vim.tbl_contains(user_keys, lhs) and not vim.tbl_contains(removed_keys, lhs) then
           table.insert(filtered_keys, key)
         end
       end
       map.key = filtered_keys
       return not vim.tbl_isempty(map.key)
     else
-      return not vim.tbl_contains(user_keys, map.key:lower()) and not vim.tbl_contains(removed_keys, map.key:lower())
+      local lhs = string.find(map.key:lower(), "^<.*>$") and map.key:lower() or key
+      return not vim.tbl_contains(user_keys, lhs) and not vim.tbl_contains(removed_keys, lhs)
     end
   end, M.mappings)
 
